d4fa5de213b638b437a689b0bcad522a16bf5222
BrunoDiPalma <brunocamargo2010@live.com>
2026-04-16 17:16:09 -0300

    feat: adiciona hash de senha com bcrypt

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 import { registerUser } from "../services/userService";
 import { registerUserSchema } from "../schemas/userSchema";
 
-export const register = (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response) => {
   const parsed = registerUserSchema.safeParse(req.body);
   if (!parsed.success) {
     return res.status(400).json({
       error: parsed.error.issues,
     });
   }
-  const user = registerUser(parsed.data);
+  const user = await registerUser(parsed.data);
 
   return res.status(201).json(user);
 };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,19 @@
 import { RegisterUserDTO } from "../schemas/userSchema";
 import { UserResponse } from "../types/user";
+import bcrypt from "bcrypt";
 
-export const registerUser = (data: RegisterUserDTO): UserResponse => {
-  return {
+export const registerUser = async (
+  data: RegisterUserDTO,
+): Promise<UserResponse> => {
+
+    const { nome, email, senha} = data
+
+    const senhaHash = await bcrypt.hash(senha,10)
+
+    console.log("Senha original:", senha)
+    console.log("Senha Hash:", senhaHash)
+
+    return {
     id: 1,
     nome: data.nome,
     email: data.email,
